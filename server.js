471b480687e70a0071957d062ca2a6f2e678057f
AishaRincon <100747969+AishaRincon@users.noreply.github.com>
2022-12-03 23:32:13 -0800

    sending index and show data

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 // DEPENDENCIES
-const express = require('express')
+const express = require('express');
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
-const app = express()
+const app = express();
 
 // MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.static('public'))
 
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
-})
+});
 
 // BREADS ROUTE
 const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+app.use('/breads', breadsController);
   
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
-})
+});
