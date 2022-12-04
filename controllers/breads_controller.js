94afe2e646aeb6d9c57e2d0cf02f7158b8df15d8
AishaRincon <100747969+AishaRincon@users.noreply.github.com>
2022-12-03 22:33:12 -0800

    added show views

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,16 +3,24 @@ const breads = express.Router()
 const Bread = require('../models/bread.js')
 
 // INDEX
+// http:localhost:3000/breads/1
 breads.get('/', (req, res) => {
-  res.render('index')
-// res.send(Bread)
+  res.render('Index',
+    {
+      breads: Bread,
+      title: 'Index Page'
+    }
+  )
+  // res.send(Bread)
 })
-
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-  res.send(Bread[req.params.arrayIndex])
+  res.render('Show', {
+    bread: Bread[req.params.arrayIndex]
+  })
 })
+
 
 
 module.exports = breads
--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -3,18 +3,17 @@ const React = require('react')
 function Default(html) {
   return (
     <html>
-      <head>
-        <title>Default</title>
-      </head>
-      <body>
-        <h1>HTML Rendered!</h1>
-        <div className="container">
-            {html.children}
-        </div>
-      </body>
+    <head>
+      <title>{html.title || 'Default'}</title>
+    </head>
+    <body>
+      <h1>HTML Rendered!</h1>
+      {html.children}
+    </body>
     </html>
   )
 }
+
 
 module.exports = Default
 
